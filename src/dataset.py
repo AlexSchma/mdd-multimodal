0cd8d0d953d8fc67b5dc25d01b7c797895dcd7d1
fr30 <fjakub.iia@gmail.com>
2025-02-13 12:53:19 +0100

    Add labels for datasets

--- src/dataset.py
+++ src/dataset.py
@@ -24,26 +24,27 @@ class InMemoryRESTfMRIDataset(Dataset):
     ):
         super(InMemoryRESTfMRIDataset, self).__init__()
         self.cache_path = os.path.join("cache", data_dir)
+        metadata = pd.read_csv(metadata_path)
 
         if self._cache_exists():
             self.edge_indices, self.node_features = self._load_cache()
         else:
             self.edge_indices, self.node_features = self._preprocess_raw_signals(
-                metadata_path, data_dir
+                metadata, data_dir
             )
             self._save_cache()
 
         self.num_samples = self.node_features.shape[0]
         self.num_nodes = self.node_features.shape[1]
+        self.labels = torch.tensor(metadata.label.values)
 
     def __len__(self):
         return self.num_samples
 
     def __getitem__(self, idx):
-        return self.edge_indices[idx], self.node_features[idx]
+        return self.edge_indices[idx], self.node_features[idx], self.labels[idx]
 
-    def _preprocess_raw_signals(self, metadata_path, data_dir):
-        metadata = pd.read_csv(metadata_path)
+    def _preprocess_raw_signals(self, metadata, data_dir):
         adj_list = []
         node_features_list = []
         for id in metadata.subID:
@@ -121,7 +122,6 @@ class RESTsMRIDataset(Dataset):
 
     def __init__(
         self,
-        metadata_path="./REST-meta-MDD/metadata.csv",
         data_dir="./REST-meta-MDD/REST-meta-MDD-VBM-Phase1-Sharing",
         splits=["wc1"],  # Can be any combination of allowed splits (["wc1", "mwc1"...])
         normalize=True,  # Normalize images
@@ -130,7 +130,6 @@ class RESTsMRIDataset(Dataset):
         super(RESTsMRIDataset, self).__init__()
         splits = sorted(splits)
         self.cache_path = os.path.join("cache", data_dir, "-".join(splits))
-        print(self.cache_path)
         self.normalize = normalize
         self.dtype = dtype
 
@@ -149,24 +148,26 @@ class InMemoryRESTsMRIDataset(RESTsMRIDataset):
         dtype=np.float32,  # Reduce to np.float16 to save memory
     ):
         super(InMemoryRESTsMRIDataset, self).__init__(
-            metadata_path, data_dir, splits, normalize, dtype
+            data_dir, splits, normalize, dtype
         )
+        metadata = pd.read_csv(metadata_path)
+
         if self._cache_exists():
             self.data = self._load_cache()
         else:
-            self.data = self._preprocess_images(metadata_path, data_dir, splits)
+            self.data = self._preprocess_images(metadata, data_dir, splits)
             self._save_cache()
 
         self.num_samples = self.data.shape[0]
+        self.labels = torch.tensor(metadata.label.values)
 
     def __len__(self):
         return self.num_samples
 
     def __getitem__(self, idx):
-        return self.data[idx]
+        return self.data[idx], self.labels[idx]
 
-    def _preprocess_images(self, metadata_path, data_dir, splits):
-        metadata = pd.read_csv(metadata_path)
+    def _preprocess_images(self, metadata, data_dir, splits):
         data = np.zeros(
             self._get_data_shape(metadata, data_dir, splits), dtype=self.dtype
         )
@@ -215,13 +216,13 @@ class LazyRESTsMRIDataset(RESTsMRIDataset):
         normalize=True,  # Normalize images
         dtype=np.float32,  # Reduce to save memory
     ):
-        super(LazyRESTsMRIDataset, self).__init__(
-            metadata_path, data_dir, splits, normalize, dtype
-        )
-        self._load_ids(metadata_path)
+        super(LazyRESTsMRIDataset, self).__init__(data_dir, splits, normalize, dtype)
+        metadata = pd.read_csv(metadata_path)
+        self.ids = metadata.subID
+        self.labels = torch.tensor(metadata.label.values)
 
         if not self._cache_exists():
-            self._create_cache(metadata_path, data_dir, splits)
+            self._create_cache(metadata, data_dir, splits)
 
         self.num_samples = len(self.ids)
 
@@ -231,17 +232,15 @@ class LazyRESTsMRIDataset(RESTsMRIDataset):
     def __getitem__(self, idx):
         filename = self.ids[idx]
         data = np.load(os.path.join(self.cache_path, f"{filename}.npy"))
-        return torch.tensor(data, dtype=NP_TO_TORCH_DTYPES[self.dtype])
+        return (
+            torch.tensor(data, dtype=NP_TO_TORCH_DTYPES[self.dtype]),
+            self.labels[idx],
+        )
 
-    def _load_ids(self, metadata_path):
-        metadata = pd.read_csv(metadata_path)
-        self.ids = metadata.subID
-
-    def _create_cache(self, metadata_path, data_dir, splits):
+    def _create_cache(self, metadata, data_dir, splits):
         if not os.path.exists(self.cache_path):
             os.makedirs(self.cache_path)
 
-        metadata = pd.read_csv(metadata_path)
         dshape = self._get_data_shape(metadata, data_dir, splits)[1:]
 
         for id in self.ids:
